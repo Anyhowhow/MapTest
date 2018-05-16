eebf07993f37b42f7873a3f82d0a06fcc3772d8a
wlh <15608070136@163.com>
2018-05-16 15:00:16 +0800

    华为测试通过，AS模拟器不通过

--- app/src/main/java/com/wlhlearning/maptest/MainActivity.java
+++ app/src/main/java/com/wlhlearning/maptest/MainActivity.java
@@ -124,21 +124,21 @@ public class MainActivity extends AppCompatActivity {
     public class MyLocationListener implements BDLocationListener{
         @Override
         public void onReceiveLocation(BDLocation location){
-//            StringBuilder currentPosition = new StringBuilder();
-//            currentPosition.append("纬度:").append(location.getLatitude()).append("\n");
-//            currentPosition.append("经度:").append(location.getLongitude()).append("\n");
-//            currentPosition.append("国家:").append(location.getCountry()).append("\n");
-//            currentPosition.append("省:").append(location.getProvince()).append("\n");
-//            currentPosition.append("市:").append(location.getCity()).append("\n");
-//            currentPosition.append("区:").append(location.getDistrict()).append("\n");
-//            currentPosition.append("街道:").append(location.getStreet()).append("\n");
-//            currentPosition.append("定位方式:");
-//            if (location.getLocType()==BDLocation.TypeGpsLocation){
-//                currentPosition.append("GPS");
-//            }else if(location.getLocType()==BDLocation.TypeNetWorkLocation){
-//                currentPosition.append("网络");
-//            }
-//            position_text.setText(currentPosition);
+            StringBuilder currentPosition = new StringBuilder();
+            currentPosition.append("纬度:").append(location.getLatitude()).append("\n");
+            currentPosition.append("经度:").append(location.getLongitude()).append("\n");
+            currentPosition.append("国家:").append(location.getCountry()).append("\n");
+            currentPosition.append("省:").append(location.getProvince()).append("\n");
+            currentPosition.append("市:").append(location.getCity()).append("\n");
+            currentPosition.append("区:").append(location.getDistrict()).append("\n");
+            currentPosition.append("街道:").append(location.getStreet()).append("\n");
+            currentPosition.append("定位方式:");
+            if (location.getLocType()==BDLocation.TypeGpsLocation){
+                currentPosition.append("GPS");
+            }else if(location.getLocType()==BDLocation.TypeNetWorkLocation){
+                currentPosition.append("网络");
+            }
+            position_text.setText(currentPosition);
             if (location.getLocType()==BDLocation.TypeGpsLocation || location.getLocType()==BDLocation.TypeNetWorkLocation){
                 navigateTo(location);
             }
